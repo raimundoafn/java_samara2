76f36fbbf0ae5e8f5bd6d6b16a909d18976ab9fb
lucas cardoso cst <lucascardosocst@gmail.com>
2025-11-07 23:43:06 -0300

    Atualizando Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,37 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Usuario {
     private String nome;
     private String email;
-    private List<Playlist> playlists;
 
     public Usuario(String nome, String email) {
         this.nome = nome;
         this.email = email;
-        this.playlists = new ArrayList<>();
     }
 
-    public String getNome() { return nome; }
-    public String getEmail() { return email; }
-
-    public void criarPlaylist(String nome) {
-        playlists.add(new Playlist(nome));
+    public String getNome() {
+        return nome;
     }
 
-    public Playlist getPlaylist(String nome) {
-        for (Playlist p : playlists) {
-            if (p.getNome().equalsIgnoreCase(nome)) {
-                return p;
-            }
-        }
-        return null;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public void listarPlaylists() {
-        System.out.println("Playlists de " + nome + ":");
-        for (Playlist p : playlists) {
-            System.out.println("- " + p.getNome());
-        }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuário: " + nome + " | Email: " + email;
     }
 }
